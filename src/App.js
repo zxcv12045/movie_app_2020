68b742159e861c420a0bfd1125d5486c978c4495
yedogwalwal <zxcv12045@naver.com>
2020-09-18 16:20:33 +0900

    Edit App.js, readme.md

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+function Food({fav}) {
+  return (
+  <h3>I love {fav}</h3>
+  );
+}
+
 function App() {
-  // return (<div className="App"/>);
-  return <div>Hello</div>;
+  return (
+  <div>
+    <h1>Hello</h1>
+    <Food fav="김치"/>
+    <Food fav="치킨"/>
+    <Food fav="햄버거"/>
+    <Food fav="피자"/>
+    <Food fav="콜라"/>
+  </div>
+  );
 }
 
 export default App;
